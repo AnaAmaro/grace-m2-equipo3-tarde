dd4e24b26ce2e05523df645377eb0736f820f986
Ana Amaro <amarovazquezana@gmail.com>
2019-09-06 20:53:30 +0200

    update design

--- _src/assets/js/design.js
+++ _src/assets/js/design.js
@@ -1,45 +1,46 @@
-const previewCard = document.querySelector(".js-palettecontainer");
+// eslint-disable-next-line strict
+const previewCard = document.querySelector('.js-palettecontainer');
 
 function createPaletteSelectorFunction(classPalette) {
   return function() {
-    previewCard.className = `js-card ${classPalette}`;
+    previewCard.className = `card ${classPalette}`;
   };
 }
 
-const selectPalette1 = createPaletteSelectorFunction("palette1");
-const selectPalette2 = createPaletteSelectorFunction("palette2");
-const selectPalette3 = createPaletteSelectorFunction("palette3");
+const selectPalette1 = createPaletteSelectorFunction('palette1');
+const selectPalette2 = createPaletteSelectorFunction('palette2');
+const selectPalette3 = createPaletteSelectorFunction('palette3');
 
 const ratio1 = document
-  .querySelector("#option1")
-  .addEventListener("change", selectPalette1);
+  .querySelector('#option1')
+  .addEventListener('change', selectPalette1);
 const ratio2 = document
-  .querySelector("#option2")
-  .addEventListener("change", selectPalette2);
+  .querySelector('#option2')
+  .addEventListener('change', selectPalette2);
 const ratio3 = document
-  .querySelector("#option3")
-  .addEventListener("change", selectPalette3);
+  .querySelector('#option3')
+  .addEventListener('change', selectPalette3);
 
 // change typography function
 
-const previewCardTypo = document.querySelector(".js-typocontainer");
+const previewCardTypo = document.querySelector('.js-typocontainer');
 
 function createTypographySelectorFunction(classTypography) {
   return function() {
-    previewCardTypo.className = `js-card ${classTypography}`;
+    previewCardTypo.className = `js-typocontainer ${classTypography}`;
   };
 }
 
-const selectTypography1 = createTypographySelectorFunction("typography1");
-const selectTypography2 = createTypographySelectorFunction("typography2");
-const selectTypography3 = createTypographySelectorFunction("typography3");
+const selectTypography1 = createTypographySelectorFunction('typography1');
+const selectTypography2 = createTypographySelectorFunction('typography2');
+const selectTypography3 = createTypographySelectorFunction('typography3');
 
 const ratioTypo1 = document
-  .querySelector("#option1_typo")
-  .addEventListener("change", selectTypography1);
+  .querySelector('#option1_typo')
+  .addEventListener('change', selectTypography1);
 const ratioTypo2 = document
-  .querySelector("#option2_typo")
-  .addEventListener("change", selectTypography2);
+  .querySelector('#option2_typo')
+  .addEventListener('change', selectTypography2);
 const ratioTypo3 = document
-  .querySelector("#option3_typo")
-  .addEventListener("change", selectTypography3);
+  .querySelector('#option3_typo')
+  .addEventListener('change', selectTypography3);
